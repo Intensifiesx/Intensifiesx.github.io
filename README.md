3f9a66906ae0f29463f38d507b606e249d8efe7a
Zain <55933220+Intensifiesx@users.noreply.github.com>
2024-11-23 20:39:25 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Intensifiesx.github.io
+# ReactWebsite
+ 
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,15 @@
+// Description: This is the contact page
+
+// Import libraries
+import React from "react";
+
+export default function Contact() {
+    return (
+        <>
+            <div>HIIIIIIII</div>
+            <div className="bottom-text" style={{ marginBottom: "-290px", backgroundColor: "black", color: "white", padding: "15px" }}>
+                Made by Zain
+            </div>
+        </>
+    );
+}
