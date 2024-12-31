f5ce759571c030ce740deb775babfce4d8af4e33
Zain <55933220+Intensifiesx@users.noreply.github.com>
2024-12-31 02:38:12 -0800

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in src
         "./public/index.html", // If you have an index.html file
-        "./src/Pages/CSS/**/*.{css}", // Include any CSS files in Pages/CSS
+        "./src/Pages/CSS/**/*.css", // Include any CSS files in Pages/CSS
     ],
     theme: {
         extend: {
